51d9ed37ba8bdbed1f212828bd63ab8a13136f21
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2022-10-26 13:35:32 -0400

    changes

--- googlesheets.py
+++ googlesheets.py
@@ -1,9 +1,29 @@
 import gspread
+import gspread_dataframe as gd
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 gc = gspread.service_account(filename = 'apex-gaming-leaderboard-6c333513634d.json')
 
-gsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1xaPFDqGpuA0t77mZ6CKpcEmaFoBC6klrS6pumivjlV4/edit#gid=0')
-mydata = gsheet.sheet1.get_all_records()
+gsheet = gc.open_by_key('10FIaM2SmoGZZIODEm5acRNNmiIrPl4seuyfKIjtDPkQ')
+all_worksheets = gsheet.worksheets()
 
-print(mydata)
+all_players = {}
+
+for worksheet in all_worksheets:
+    if worksheet.title == 'Leaderboard':
+        print("This is the Leaderboard")
+    else:
+        for record in worksheet.get_all_records():
+            if record['Name'] in all_players:
+                all_players[record['Name']] += record['Leaderboard Points']
+            else:
+                all_players[record['Name']] = record['Leaderboard Points']
+
+sorted_points = sorted(all_players.items(), key=lambda x: x[1], reverse=True)
+
+df = pd.DataFrame(sorted_points, columns=['Name', 'Leaderboard Points'])
+
+print(df)
+
+gd.set_with_dataframe(gsheet.sheet1, df)

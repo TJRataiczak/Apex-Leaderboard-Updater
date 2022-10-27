c1b578ea3b69bf075b1112d53aeb7d5a274b9ebb
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2022-10-27 09:57:06 -0400

    updates

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+import gspread
+import gspread_dataframe as gd
+from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+
+gc = gspread.service_account(filename = 'apex-gaming-leaderboard-6c333513634d.json')
+
+gsheet = gc.open_by_key('10FIaM2SmoGZZIODEm5acRNNmiIrPl4seuyfKIjtDPkQ')
+all_worksheets = gsheet.worksheets()
 
 all_players = {}
 
-xl = pd.ExcelFile('Apex Gaming Season 2 Leaderboard.xlsx')
-
-all_sheets = xl.sheet_names
-
-for current_sheet in all_sheets:
-    if current_sheet != 'Leaderboard':
-        first_event = xl.parse(current_sheet, usecols=('Rank', 'Name', 'Points'))
-        for player in zip(first_event['Name'], first_event["Points"]):
-            if player[0] in all_players:
-                all_players[player[0]] += player[1]
+for worksheet in all_worksheets:
+    if worksheet.title == 'Leaderboard':
+        print("This is the Leaderboard")
+    else:
+        for record in worksheet.get_all_records():
+            if record['Name'] in all_players:
+                all_players[record['Name']] += record['Leaderboard Points']
             else:
-                all_players[player[0]] = player[1]
+                all_players[record['Name']] = record['Leaderboard Points']
 
-fixed_list = sorted(all_players.items(), key=lambda x: x[1], reverse=True)
+sorted_points = sorted(all_players.items(), key=lambda x: x[1], reverse=True)
 
-for player in fixed_list:
-    print(player)
+df = pd.DataFrame(sorted_points, columns=['Name', 'Leaderboard Points'])
+
+print(df)
+
+gd.set_with_dataframe(gsheet.sheet1, df)

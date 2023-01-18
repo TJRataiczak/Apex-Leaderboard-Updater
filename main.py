787c77fb0beff1023ba2adbbb54910b257321979
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-01-18 13:58:35 -0500

    updates to players

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ class Player:
         self.name = name
         self.points = points
         self.qualified = False
+        self.highest_rank = 4000
 
     def add_points(self, more_points):
         self.points += more_points
     
     def update_qualification(self, qual):
         self.qualified = qual
+    
+    def update_highest_placing(self, rank):
+        self.highest_rank = rank
 
 import gspread
 import gspread_dataframe as gd
@@ -25,6 +29,7 @@ all_players = {}
 
 all_players['Randy Uebbing'] = Player('Randy Uebbing', 0)
 all_players['Randy Uebbing'].update_qualification(True)
+
 
 for worksheet in all_worksheets:
     if worksheet.title == 'Leaderboard':
@@ -43,25 +48,31 @@ for worksheet in all_worksheets:
             elif record['Rank'] in ranks_qualified and all_players[record['Name']].qualified == False:
                 all_players[record['Name']].update_qualification(True)
                 ranks_qualified.remove(record['Rank'])
+            
+            if record['Rank'] < all_players[record['Name']].highest_rank:
+                all_players[record['Name']].update_highest_placing(record['Rank'])
 
 player_names = []
 player_points = []
 player_qualified = []
+player_highest_placing = []
 
 for person in all_players:
     player_names.append(all_players[person].name)
     player_points.append(all_players[person].points)
     player_qualified.append('Yes' if all_players[person].qualified == True else 'No')
+    player_highest_placing.append(all_players[person].highest_rank)
 
 df_dict = {'Name': player_names,
             'Points': player_points,
-            'Qualified': player_qualified}
+            'Qualified': player_qualified,
+            'Highest Placing': player_highest_placing}
 
 df = pd.DataFrame(df_dict)
 
-gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by='Points', ascending=False), col=2)
+gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by=['Points', 'Highest Placing'], ascending=[False, True]), col=2)
 
-format_cell_range(gsheet.sheet1, 'A:D', NORMAL_FORMAT)
+format_cell_range(gsheet.sheet1, 'A:F', NORMAL_FORMAT)
 
 player_names.sort()
 
--- nonqualified_leaderboard.py
+++ nonqualified_leaderboard.py
@@ -3,12 +3,16 @@ class Player:
         self.name = name
         self.points = points
         self.qualified = False
+        self.highest_rank = 4000
 
     def add_points(self, more_points):
         self.points += more_points
     
     def update_qualification(self, qual):
         self.qualified = qual
+    
+    def update_highest_placing(self, rank):
+        self.highest_rank = rank
 
 import gspread
 import gspread_dataframe as gd
@@ -22,6 +26,9 @@ gsheet = gc.open_by_key('10FIaM2SmoGZZIODEm5acRNNmiIrPl4seuyfKIjtDPkQ')
 all_worksheets = gsheet.worksheets()
 
 all_players = {}
+
+all_players['Randy Uebbing'] = Player('Randy Uebbing', 0)
+all_players['Randy Uebbing'].update_qualification(True)
 
 
 for worksheet in all_worksheets:
@@ -41,26 +48,32 @@ for worksheet in all_worksheets:
             elif record['Rank'] in ranks_qualified and all_players[record['Name']].qualified == False:
                 all_players[record['Name']].update_qualification(True)
                 ranks_qualified.remove(record['Rank'])
+            
+            if record['Rank'] < all_players[record['Name']].highest_rank:
+                all_players[record['Name']].update_highest_placing(record['Rank'])
 
 player_names = []
 player_points = []
 player_qualified = []
+player_highest_placing = []
 
 for person in all_players:
     if all_players[person].qualified == False:
         player_names.append(all_players[person].name)
         player_points.append(all_players[person].points)
         player_qualified.append('Yes' if all_players[person].qualified == True else 'No')
+        player_highest_placing.append(all_players[person].highest_rank)
 
 df_dict = {'Name': player_names,
             'Points': player_points,
-            'Qualified': player_qualified}
+            'Qualified': player_qualified,
+            'Highest Placing': player_highest_placing}
 
 df = pd.DataFrame(df_dict)
 
 gsheet = gc.open_by_key('1NV8DwCtsU6zazcElTmhIX6nhrNX4vXBNiwwh1LujYek')
 
-gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by='Points', ascending=False), col=2)
+gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by=['Points', 'Highest Placing'], ascending=[False, True]), col=2)
 
 format_cell_range(gsheet.sheet1, 'A:D', NORMAL_FORMAT)
 

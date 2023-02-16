6cbca8da1ce490a343dfa7af3c81aa35a3822710
TJ Rataiczak <69827401+TJRataiczak@users.noreply.github.com>
2023-02-16 13:43:15 -0500

    Update nonqualified_leaderboard.py

--- nonqualified_leaderboard.py
+++ nonqualified_leaderboard.py
@@ -1,18 +1,23 @@
 class Player:
-    def __init__(self, name, points):
+    def __init__(self, name, place):
         self.name = name
-        self.points = points
-        self.qualified = False
-        self.highest_rank = 4000
-
-    def add_points(self, more_points):
-        self.points += more_points
+        self.placings = [place]
+        self.wins = 0
+        self.top_finish = 0
+        self.entered_events = 0
+        self.top_finish_percentage = 0
+        self.win_percentage = 0
     
-    def update_qualification(self, qual):
-        self.qualified = qual
+    def add_placing(self, new_placing):
+        self.placings.append(new_placing)
     
-    def update_highest_placing(self, rank):
-        self.highest_rank = rank
+    def update_stats(self):
+        self.wins = self.placings.count(1)
+        for finish in [1,2,3,4,5,6,7,8]:
+            self.top_finish += self.placings.count(finish)
+        self.entered_events = len(self.placings)
+        self.win_percentage = self.wins / self.entered_events
+        self.top_finish_percentage = self.top_finish / self.entered_events
 
 import gspread
 import gspread_dataframe as gd
@@ -27,57 +32,43 @@ all_worksheets = gsheet.worksheets()
 
 all_players = {}
 
-all_players['Randy Uebbing'] = Player('Randy Uebbing', 0)
-all_players['Randy Uebbing'].update_qualification(True)
-
-
 for worksheet in all_worksheets:
     if worksheet.title == 'Leaderboard':
         print("Skipping Leaderboard Sheet")
     else:
-        ranks_qualified = [1,2]
         for record in worksheet.get_all_records():
             if record['Name'] in all_players:
-                all_players[record['Name']].add_points(record['Leaderboard Points'])
+                all_players[record['Name']].add_placing(record['Rank'])
             else:
-                all_players[record['Name']] = Player(record['Name'], record['Leaderboard Points'])
-            
-            if record['Rank'] in ranks_qualified and all_players[record['Name']].qualified == True:
-                for i in range(len(ranks_qualified)):
-                    ranks_qualified[i] += 1
-            elif record['Rank'] in ranks_qualified and all_players[record['Name']].qualified == False:
-                all_players[record['Name']].update_qualification(True)
-                ranks_qualified.remove(record['Rank'])
-            
-            if record['Rank'] < all_players[record['Name']].highest_rank:
-                all_players[record['Name']].update_highest_placing(record['Rank'])
+                all_players[record['Name']] = Player(record['Name'], record['Rank'])
 
-player_names = []
-player_points = []
-player_qualified = []
-player_highest_placing = []
+player_name = []
+player_wins = []
+player_top_finish = []
+player_events_entered = []
+player_win_percentage = []
+player_top_finish_percentage = []
 
 for person in all_players:
-    if all_players[person].qualified == False:
-        player_names.append(all_players[person].name)
-        player_points.append(all_players[person].points)
-        player_qualified.append('Yes' if all_players[person].qualified == True else 'No')
-        player_highest_placing.append(all_players[person].highest_rank)
+    all_players[person].update_stats()
+    player_name.append(all_players[person].name)
+    player_wins.append(all_players[person].wins)
+    player_events_entered.append(all_players[person].entered_events)
+    player_top_finish.append(all_players[person].top_finish)
+    player_win_percentage.append(all_players[person].win_percentage)
+    player_top_finish_percentage.append(all_players[person].top_finish_percentage)
 
-df_dict = {'Name': player_names,
-            'Points': player_points,
-            'Qualified': player_qualified,
-            'Highest Placing': player_highest_placing}
+df_dict = {'Name': player_name,
+            'Wins': player_wins,
+            'Top 8s': player_top_finish,
+            'Entered': player_events_entered, 
+            'Win %': player_win_percentage,
+            'Top 8 %': player_top_finish_percentage}
 
 df = pd.DataFrame(df_dict)
 
 gsheet = gc.open_by_key('1NV8DwCtsU6zazcElTmhIX6nhrNX4vXBNiwwh1LujYek')
 
-gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by=['Points', 'Highest Placing'], ascending=[False, True]), col=2)
+gd.set_with_dataframe(gsheet.sheet1, df.sort_values(by=['Top 8s', 'Wins'], ascending=[False, False]))
 
-format_cell_range(gsheet.sheet1, 'A:D', NORMAL_FORMAT)
-
-player_names.sort()
-
-for name in player_names:
-    print(name)
+format_cell_range(gsheet.sheet1, 'A:F', NORMAL_FORMAT)
